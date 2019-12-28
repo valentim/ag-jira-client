e4fccf376c158419f5e402a64cfb585508c2e15f
Thiago Valentim <thiago@criawebs.com.br>
2019-12-28 15:37:05 -0300

    chore: implement an example of CFD

--- src/app/Pages/Dashboard.tsx
+++ src/app/Pages/Dashboard.tsx
@@ -1,10 +1,23 @@
 import React, { Component } from "react";
 import Layout from "../Partials/Layout";
+import CFD from 'ag-cumulative-flow'
 
 export default class Dashboard extends Component {
     render() {
+        const cumulativeFlowData = {
+            Backlog: [{
+                x: 1570503600, y: 2
+            }],
+            Done: [{
+                x: 1570417200, y: 1
+            }]
+        };
+
         return (
-            <Layout><div>Dashboard</div></Layout>
+            <Layout>
+                <div>Dashboard</div>
+                <CFD data={cumulativeFlowData}></CFD>
+            </Layout>
         )
     }
 }
