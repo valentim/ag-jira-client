ba6df97f0f101646acd46ab65a1483dde42c5464
Thiago Valentim <thiago@criawebs.com.br>
2019-12-28 11:42:47 -0300

    chore: add the dashboard page

---
+++ src/app/Pages/Dashboard.tsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import Layout from "../Partials/Layout";
+
+export default class Dashboard extends Component {
+    render() {
+        return (
+            <Layout><div>Dashboard</div></Layout>
+        )
+    }
+}
